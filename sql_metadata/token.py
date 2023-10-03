61f5dad9c83d7531c4b2d4ff3dc1550e7f543794
Dylan Scott <dscott@hex.tech>
2023-10-02 19:29:00 -0700

    try to distinguish pivot operator

--- sql_metadata/token.py
+++ sql_metadata/token.py
@@ -264,6 +264,7 @@ class SQLToken:  # pylint: disable=R0902, R0904
             and self.last_keyword_normalized in TABLE_ADJUSTMENT_KEYWORDS
             and self.previous_token.normalized not in ["AS", "WITH"]
             and self.normalized not in ["AS", "SELECT", "IF", "SET", "WITH"]
+            and not self.last_keyword_pivot_operator
         )
 
     @property
@@ -337,6 +338,24 @@ class SQLToken:  # pylint: disable=R0902, R0904
             self.last_keyword_normalized == "INTO"
             and self.previous_token.is_punctuation
         )
+
+    @property
+    def is_pivot_operator(self) -> bool:
+        if self.normalized in {"PIVOT", "UNPIVOT"}:
+            next_token = self.next_token_not_comment
+            return next_token is not None and next_token.is_left_parenthesis
+        else:
+            return False
+
+    @property
+    def last_keyword_pivot_operator(self) -> bool:
+        if self.last_keyword_normalized in {"PIVOT", "UNPIVOT"}:
+            last_keyword_token = self.find_nearest_token(self.last_keyword)
+            return (
+                last_keyword_token is not None and last_keyword_token.is_pivot_operator
+            )
+        else:
+            return False
 
     @property
     def is_potential_alias(self) -> bool:
